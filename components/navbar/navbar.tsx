0e64c12e8ad490f98f7613e6a9412a0d09563004
Syammed2429 <43319549+Syammed2429@users.noreply.github.com>
2024-10-23 17:46:16 +0100

    added the navbar

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+import icon from "@/public/icons/ploonet-icon.svg";
+import globeIcon from "@/public/icons/globe-icon.svg";
+const NavbarComponent = () => {
+  return (
+    <div className="fixed top-0 w-full bg-black backdrop-blur-sm z-50 ">
+      <div className="flex items-center justify-around py-10">
+        <Image src={icon} alt="icon" />
+        <ol className="flex items-center gap-20">
+          <li>스튜디오</li>
+          <li>워크센터</li>
+          <li>손비서</li>
+          <li>메타휴먼</li>
+          <li>영상제작소</li>
+          <li>공지사항</li>
+          <li>제휴문의</li>
+        </ol>
+        <div className="flex items-center gap-14">
+          <div>로그인</div>
+          <Image src={globeIcon} alt="globeIcon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarComponent;
---
+++ containers/layout/layout.tsx
@@ -0,0 +1,13 @@
+import NavbarComponent from "@/components/navbar/navbar";
+import React from "react";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <NavbarComponent />
+      <div className="">HomeLayout</div>
+    </div>
+  );
+};
+
+export default HomeLayout;
