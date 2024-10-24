401c4c1398af91a3252fcec45fdf9d4350fbe0e6
Syammed2429 <43319549+Syammed2429@users.noreply.github.com>
2024-10-24 15:48:53 +0100

    added the missed text

--- containers/making-process/making-process-container.tsx
+++ containers/making-process/making-process-container.tsx
@@ -13,6 +13,16 @@ const MakingProcessContainer = () => {
         <PloonetFirstCardComponent />
         <PloonetQuickserveCardComponent />
       </div>
+      <div className="text-center text-gray-400 py-12">
+        <p>
+          * 2~3분, 표준 견적에 따르는 일반 영상편집에 한정되며, 모션그래픽 등
+          전문 작업이 필요할 경우 작업 기간은 조정될 수 있습니다.
+        </p>
+        <p>
+          ** 영상의 길이, 영상 제작에 필요한 수준에 따라 빠른 작업이 불가할 수
+          있으며, 클라이언트와의 협의를 통해 작업 기간을 조정할 수 있습니다.
+        </p>
+      </div>
     </div>
   );
 };
