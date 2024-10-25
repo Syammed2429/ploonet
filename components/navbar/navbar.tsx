808d59b77504ebbc6833937f60be2a3e232c94f8
Dada Khalandar <kerry.blig12@gmail.com>
2024-10-25 17:06:01 +0800

    refactor : added the hover dot

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,22 +1,40 @@
+'use client'
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import icon from "@/public/icons/ploonet-icon.svg";
 import globeIcon from "@/public/icons/globe-icon.svg";
 import { MobileNavbarSheet } from "./mobile-navbar-sheet";
+import { motion } from "framer-motion";
+import { mockData } from "@/lib/mockdata";
 
 const NavbarComponent = () => {
+  const { navbarItems } = mockData;
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
   return (
     <nav className="fixed top-0 w-full bg-black backdrop-blur-sm z-50">
-      <div className="hidden xl:flex items-center justify-around py-7">
+      <div className="hidden xl:flex items-center justify-around py-7 px-5">
         <Image src={icon} alt="icon" />
         <ol className="flex items-center gap-20">
-          <li>스튜디오</li>
-          <li>워크센터</li>
-          <li>손비서</li>
-          <li>메타휴먼</li>
-          <li>영상제작소</li>
-          <li>공지사항</li>
-          <li>제휴문의</li>
+          {navbarItems?.map((item, index) => (
+            <li 
+              key={item.id} 
+              className="relative hover:cursor-pointer"
+              onMouseEnter={() => setHoveredIndex(index)} 
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              {item.title}
+              {hoveredIndex === index && (
+                <motion.div 
+                  className="bg-gradient-to-r from-[#FF4E83] to-[#FFBB54] w-2 md:w-4 h-2 md:h-4 rounded-full absolute left-1/2 transform -translate-x-1/2 mt-1"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                />
+              )}
+            </li>
+          ))}
         </ol>
         <div className="flex items-center gap-14">
           <div>로그인</div>
