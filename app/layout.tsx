06098a6abb40514c46547b9fae2d251d1eeb82f3
Syammed2429 <43319549+Syammed2429@users.noreply.github.com>
2024-10-25 12:18:42 +0100

    feat : added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const getPretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${getPretendard.variable}  antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
