251988bc085c2d57e57ca0a99dde4370f3c4ac5d
Dada Khalandar <kerry.blig12@gmail.com>
2024-10-25 11:45:32 +0800

    chore : made responsiveness for the contact details

--- components/contact/contact-card.tsx
+++ components/contact/contact-card.tsx
@@ -22,18 +22,18 @@ const ContactCard: FC<ContactCardProps> = ({
   return (
     <div>
       <Card className="px-10 bg-[#141414]  py-5  border border-[#ffffff26]">
-        <CardHeader className="p-0 space-y-0  text-lg">
-          <CardTitle className="flex items-center justify-between text-lg text-white  font-bold">
+        <CardHeader className="p-0 space-y-0 text-base md:text-lg">
+          <CardTitle className="flex items-center justify-between text-base md:text-lg text-white  font-bold">
             <div className="opacity-60">{header}</div>
             {showArrow && <Image src={chevronDown} alt="chvronDown" />}
           </CardTitle>
-          <CardDescription className="text-white opacity-60 text-lg">
+          <CardDescription className="text-white opacity-60 text-base md:text-lg">
             {description}
           </CardDescription>
         </CardHeader>
         <CardContent className="p-0">
           {options?.length && (
-            <div className="pb-16 mt-6 text-white opacity-60 text-lg">
+            <div className="pb-7 md:pb-16 mt-6 text-white opacity-60 text-base md:text-lg">
               {options?.map((option: Options) => (
                 <div key={option?.value} className="">
                   <span>{option?.label}</span>
--- containers/contact/contact-container.tsx
+++ containers/contact/contact-container.tsx
@@ -48,34 +48,57 @@ const data = [
 
 const ContactContainer = () => {
   return (
-      <div className="space-y-6">
-        {data?.map((data) => (
-          <div key={data?.id}>
-            <ContactCard
-              header={data?.title}
-              description={data?.description}
-              showArrow={data?.showArrow}
-              options={data?.options}
-            />
-          </div>
-        ))}
-        <div className="text-[#FF6D51] text-base font-normal">
-          * 참고할만한 영상이나 원하시는 영상과 유사한 영상의 링크 주소를
-          적어주시면 더 정확히 안내해드리겠습니다.
+    <div className="space-y-6">
+      <div className="flex flex-col items-center justify-center gap-6 text-center">
+
+        <span className="mb-8 bg-clip-text text-xl md:text-2xl leading-9 font-bold text-transparent bg-gradient-to-r from-[#FF4E83] to-[#FFBB54]">
+          영상 제작 문의
+        </span>
+        <div className='flex flex-col md:flex-row items-center justify-center  text-2xl lg:text-[2.75rem] font-bold'>
+          <p >영상 제작이 필요하다면,</p>
+          <p className="font-light">지금 문의 주세요.</p>
         </div>
-        <div className="flex justify-between items-center pt-36 pb-80">
-          <div className="flex  items-center gap-3">
-            <Image src={checkIcon} alt="checkIcon" />
-            <div className="opacity-60 m-0 p-0">
-              <span className=" underline">개인정보처리방침 </span>
-              <span className="no-underline">에 동의합니다.</span>
-            </div>
-          </div>
-          <Button className="bg-[#FF6D51] hover:bg-[#FF6D21] text-white">
-            문의 보내기
-          </Button>
+        <div className='text-center text-base font-normal lg:text-2xl md:font-bold opacity-60'>
+          여러분이 필요한 영상을 빠르게 제작하기 위해 플루닛 영상제작소가
+          즉시 연락 드리겠습니다.
+        </div>
+        <div className="text-base md:text-lg font-normal text-[#666666]">
+          <p className='block md:hidden text-base font-bold'>E-MAIL</p>
+          <span className="text-white md:text-inherit">
+            henry.lim@saltlux.com
+          </span>
         </div>
       </div>
+      {data?.map((data) => (
+        <div key={data?.id}>
+          <ContactCard
+            header={data?.title}
+            description={data?.description}
+            showArrow={data?.showArrow}
+            options={data?.options}
+          />
+        </div>
+      ))}
+      <div className="flex items-start gap-0.5 text-[#FF6D51] text-xs md:text-base font-normal mt-14 md:mt-2">
+        <p>
+          *
+        </p>
+        참고할만한 영상이나 원하시는 영상과 유사한 영상의 링크 주소를
+        적어주시면 더 정확히 안내해드리겠습니다.
+      </div>
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center pt-36 pb-16 md:pb-80 space-y-16 md:space-y-0">
+        <div className="flex items-center gap-3">
+          <Image src={checkIcon} alt="checkIcon" />
+          <div className="opacity-60 m-0 p-0 text-sm md:text-xl ">
+            <span className=" underline">개인정보처리방침 </span>
+            <span className="no-underline">에 동의합니다.</span>
+          </div>
+        </div>
+        <Button className="bg-[#FF6D51] hover:bg-[#FF6D21] text-white p-[1rem_1.75rem]">
+          문의 보내기
+        </Button>
+      </div>
+    </div>
   );
 };
 
